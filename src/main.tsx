c20efb44b8c4c0bae3874913bd30a19b88f0cdcc
Anna Agoha <agohaanna@gmail.com>
2025-11-25 17:58:38 -0800

    trying something

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
+// src/main.tsx or src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
+import './index.css'; // if you have global styles
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <HashRouter>
       <App />
