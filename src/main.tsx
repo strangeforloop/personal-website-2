0ea190421777b5d32d1703c3a7670495f1505042
Anna Agoha <agohaanna@gmail.com>
2025-11-25 17:59:56 -0800

    .

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
-// src/main.tsx or src/main.jsx
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
-import App from './App';
-import './index.css'; // if you have global styles
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </React.StrictMode>,
-);
+)
+
