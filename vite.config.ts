482aeb5dcaef06dc4ca5ed7e890c6aefc90d0c86
Anna Agoha <agohaanna@gmail.com>
2025-11-25 17:44:38 -0800

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/personal-website-2 ',
+  base: '/personal-website-2/',
 })
 
