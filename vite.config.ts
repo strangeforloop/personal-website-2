87f08da291e368938c81e97343396e4a1b9bdd6d
Anna Agoha <agohaanna@gmail.com>
2026-04-07 07:52:10 -0700

    update base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // GitHub Pages project site path: https://<user>.github.io/Portfolio_Website/
-  base: '/Portfolio_Website/',
+  // User site root path: https://strangeforloop.github.io/
+  base: '/',
 })
 
